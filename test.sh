979de05b8a208f817f519d374c12a98b7c616f09
Andrew Backes <rbackes@ucla.edu>
2016-05-31 22:13:00 -0700

    fixed test runner

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd "$( dirname "$0" )"
 
-packages=$(find . -type d | tail -n +2 |grep -v "git\|releases\|bin")
+packages=$(find . -type d | tail -n +2 | grep -v "git\|releases\|bin")
 
 IFS=$'\n'
 for pkg in $packages; do
     pushd $pkg
-    go test -coverprofile=.coverprofile -test.v -covermode=count || exit 1
+    ls *.go > /dev/null
+    if [ "$?" -eq  "0" ]; then
+        go test -coverprofile=.coverprofile -test.v -covermode=count || exit 1
+    fi
     popd
 done
 echo "mode: count" > total.coverprofile
